7a0864327ce79e8e42570e7ac9d7d8e28409ae72
yanith04 <132030728+yanith04@users.noreply.github.com>
2023-05-01 23:00:31 -0500

    task one

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,9 +1,11 @@
 import pygame
-
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+import random
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, CLOUD
 from dino_runner.components.dinosaur import dinosaur
 
 class Game:
+    
+
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
@@ -14,8 +16,14 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.game_speeds= 25
+        #self.cloud= CLOUD
+        self.x_pos_cloud = SCREEN_WIDTH + random.randint(800,1000)
+        self.y_pos_cloud = random.randint(50,100)
         self.player = dinosaur()
-
+        #self.clouds = []         #self.x_pos_cloud >= 100
+        
+            
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
@@ -42,11 +50,41 @@ class Game:
         pygame.display.update()
         pygame.display.flip()
 
+       
+               
+
     def draw_background(self):
         image_width = BG.get_width()
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
         self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+        image_width = CLOUD.get_width()
+        self.screen.blit(CLOUD, (self.x_pos_cloud, self.y_pos_cloud))
+        self.screen.blit(CLOUD, (image_width + self.x_pos_cloud, self.y_pos_cloud))
+       
+       # for cloud in range (1,5):
+        # self.clouds.append(CLOUD)
+       
         if self.x_pos_bg <= -image_width:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+        if  self.x_pos_cloud <= -image_width:
+            self.x_pos_cloud = SCREEN_WIDTH + random.randint(2500,3000) 
+            self.y_pos_cloud = random.randint(50,100)    
+        self.x_pos_cloud -= self.game_speeds
+        #for i in range (5):
+         #self.clouds.append(CLOUD)
+           # self.screen.blit(CLOUD, (image_width + self.x_pos_cloud, self.y_pos_cloud))
+    #frame_clouds= 0
+    #clouds = []
+    #for index in range(6):
+     #   x_pos_cloud = SCREEN_WIDTH + random.randint(800,1000)
+    #  y_pos_cloud = random.randint(50,100)
+     #   speed_cloud = random.randint(1,8)
+      #  list_clouds = [x_pos_cloud,y_pos_cloud, speed_cloud]
+       # clouds.append(list_clouds)
+
+        
+
+
+        
